83761b9fd5a9d941846d03f2191aa2f624472096
Arlosa999 <annabilancan@gmail.com>
2023-10-08 18:48:32 -0400

    Oct 8 first commit: First page is done gamepage is in progress

---
+++ logic.js
@@ -0,0 +1,5 @@
+// array of images 
+arrayOfImages=[]
+arrayOfImages.push("./BodyBuilder.png", "./Cyclist.png", "./Gymnast.png", "./HorseRider.png", "./Parasailor.png", "./RockClimber.png", "RollerBlader.png", "./TennisPlayer.png")
+console.log(arrayOfImages)
+
