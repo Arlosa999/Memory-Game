ca53604ddd3e7fad8ba8f87efa86f117ee0a0788
Arlosa999 <annabilancan@gmail.com>
2023-10-09 22:44:28 -0400

    outcomes oct 9

--- logic.js
+++ logic.js
@@ -1,15 +1,35 @@
 //create an object that will store all cards from html 
 const cards=document.querySelectorAll(".gamecard")
+// I need to create objects that represent the flipping part of the game 
+let flippedCard=false
+let firstcard, secondcard
+
 //perform a function to flip the card 
 function flipCard(card) {
-    this.classList.toggle('flip')
-    console.log("i clicked the card")
+    this.classList.add('flip')
+    // console.log("i clicked the card")
+    if(!flippedCard){
+        flippedCard=true
+        firstcard=this
+    }
+    else{
+        flippedCard=false
+        secondcard=this
+    }
+    //we need to chekc if cards match 
+    // console.log(firstcard.dataset.card===
+    //     secondcard.dataset.card) {
+    //         firstcard.removeEventListener("click", flipCard)
+    //         secondcard.removeEventListener("click", flipCard)
+    //     }
+    
 }
-for (let i=0; i<cards.length; i++){
-    i++
-    cards[i].addEventListener('click', flipCard)
 
-}
+// for (let i=0; i<cards.length; i++){
+//     i++
+//     cards[i].addEventListener('click', flipCard)
+
+// }
 
 //create an event to add event listener to the card to flip when card is clicked 
 cards.forEach(card => card.addEventListener('click',flipCard)) 
@@ -21,3 +41,4 @@ cards.forEach(card => card.addEventListener('click',flipCard))
 // let cardVariations=document.createElement('div')
 
 // }
+
