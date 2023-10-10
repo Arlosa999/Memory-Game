e5ccd54102bfac7c7b45ced735b0a04359eb3ef6
Arlosa999 <annabilancan@gmail.com>
2023-10-09 20:18:17 -0400

    cards flip

--- logic.js
+++ logic.js
@@ -1,8 +1,23 @@
-//create an object that will all cards from html 
+//create an object that will store all cards from html 
 const cards=document.querySelectorAll(".gamecard")
 //perform a function to flip the card 
-function flipCard() {
+function flipCard(card) {
     this.classList.toggle('flip')
+    console.log("i clicked the card")
 }
+for (let i=0; i<cards.length; i++){
+    i++
+    cards[i].addEventListener('click', flipCard)
+
+}
+
 //create an event to add event listener to the card to flip when card is clicked 
-cards.forEach(card => card.addEventListener('click', flipCard))
+cards.forEach(card => card.addEventListener('click',flipCard)) 
+// console.log(event.target.parentElement)
+// flipCard(event.target.parentElement)
+
+// var shuffleCards=cards.sort(()=> (Math.random()>.5)?2:-1)
+// for (var i=0; i<cards.length; i++){
+// let cardVariations=document.createElement('div')
+
+// }
