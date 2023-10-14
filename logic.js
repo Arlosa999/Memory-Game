f799cd6005371a77a78cbba23aa82a0c6fed7bf0
Arlosa999 <annabilancan@gmail.com>
2023-10-14 18:02:16 -0400

    Flipping, shuffeling, working through the win pop up window

--- logic.js
+++ logic.js
@@ -1,35 +1,12 @@
 //create an object that will store all cards from html 
 const cards=document.querySelectorAll(".gamecard")
-// array for flipping cards 
+// empty array for flipping cards 
 let guess=[]
 
-// I need to create objects that represent the flipping part of the game 
-let flippedCard=false
-let firstcard, secondcard
-
-//perform a function to flip the card 
-function flipCard(card) {
-    this.classList.add('flip')
-    // console.log("i clicked the card")
-    
-    if(!flippedCard){   
-        flippedCard=true
-        firstcard=this
-    }
-    else{
-        flippedCard=false
-        secondcard=this
-    }  
-}
+const resetButton = document.getElementById("resetbutton");
+resetButton.addEventListener("click", resetGame);
 
 // Trucking 2 guesses so only 2 cards are flipping and if match they stay front-face if doesnr they turn back 
-
-
-// for (let i=0; i<cards.length; i++){
-//     i++
-//     cards[i].addEventListener('click', flipCard)
-
-// }
 
 //create an event to add event listener to the card to flip when card is clicked 
 cards.forEach(card => card.addEventListener('click',(e)=>{      //e-means event 
@@ -38,33 +15,96 @@ console.log(e.target.parentElement)
 console.log(e.target.parentElement.dataset.id)
 if(!e.target.parentElement.classList.contains("flip")) {
     e.target.parentElement.classList.toggle("flip")  //checking if card has been flipped 
+    guess.push(e.target.parentElement.dataset.id)
 }
-
 //checking if card is flipped 
-guess.push(e.target.parentElement.dataset.id)
 console.log(guess)
 if (guess.length===2){
     if(guess[0]===guess[1]){
-        guess.pop()
-        guess.pop()  
+        guess=[] 
+        winLogic()
+        // console.log(match)
     }
-    if(guess[0]!==guess[1]){
-        document.querySelectorAll(".flip").forEach((item)=>{
-            setTimeout(()=>{item.classList.toggle("flip")}, 1000);
+    else if(guess[0]!==guess[1]){
+        console.log(guess)
+        guess.forEach((item)=>{
+            setTimeout(()=>{
+               const elementList = document.querySelectorAll(`[data-id="${item}"]`) 
+               console.log(elementList)
+                elementList.forEach(element=>{
+                if (element.classList.contains("flip"))  {
+                    element.classList.toggle("flip")  
+                   }  
+              })               
+               // console.log(item.dataset.id)
+            }, 500); ///i need to fix something here 
         }) ///i am trying to flip not matching cards back 
-        }
-        
-        guess.pop()
-        guess.pop()
-        }}
-    ))       
+        guess=[]
+    }
+        } 
+    }   
+    ))  
+function resetGame() {
 
+    // Reset all cards to face down
+    cards.forEach(card => {
+        if (card.classList.contains("flip")) {
+            card.classList.remove("flip");
+        }
+    });
+    shuffleCards()
+}
+function shuffleCards() {
+    const cardArray = Array.from(cards);
+    cardArray.forEach(card => {
+        const randomPosition = Math.floor(Math.random() * cardArray.length);
+        card.style.order = randomPosition;
+    });
+}  
+
+function winLogic (){
+    const flippedCards=document.querySelectorAll('.flip')
+    if(flippedCards.length===cards.length){
+        const winTheGame=document.getElementById("popUpWindowWin")
+        winTheGame.style.display="block"
+        console.log("winTheGame")
+    }
+    
+}
+shuffleCards()
+//timer 
+// document.addEventListener(".resetbutton", function () {
+//     // Set the target date and time (in this example, 5 minutes)
+//     const targetTime = new getTime() + 5 * 60 * 1000;
+
+//     // Update the timer every second
+//     const timerInterval = setInterval(updateTimer, 1000);
+
+//     function updateTimer() {
+//         const currentTime = new getTime();
+//         const timeDifference = targetTime - currentTime;
+
+//         // Calculate minutes and seconds
+//         const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+//         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+//         // Display the time in the HTML
+//         document.getElementById("minutes").innerText = padZero(minutes);
+//         document.getElementById("seconds").innerText = padZero(seconds);
+
+//         // Check if the timer has reached 0
+//         if (timeDifference < 0) {
+//             clearInterval(timerInterval);
+//             alert("Time's up!");
+//         }
+//     }
+//     // Function to pad zero for single-digit minutes and seconds
+//     function padZero(value) {
+//         return value < 10 ? "0" + value : value;
+//     }
+// });
 // console.log(event.target.parentElement)
 // flipCard(event.target.parentElement)
 
-// var shuffleCards=cards.sort(()=> (Math.random()>.5)?2:-1)
-// for (var i=0; i<cards.length; i++){
-// let cardVariations=document.createElement('div')
 
-// }
 
